c093f7064e1f007a04da29f70f6c960e78c05736
SergeyBerdnikovich <atom@tut.by>
2013-04-07 15:43:19 +0300

    added model video

---
+++ app/models/video.rb
@@ -0,0 +1,3 @@
+class Video < ActiveRecord::Base
+  attr_accessible :youtube_url
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407124143) do
+ActiveRecord::Schema.define(:version => 20130407124255) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -101,5 +101,11 @@ ActiveRecord::Schema.define(:version => 20130407124143) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "videos", :force => true do |t|
+    t.string   "youtube_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
